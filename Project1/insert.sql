d6380f53e61add0ba6d3dd52724d7ab468d81195
Jung Jo Hyung <epikjjh@gamil.com>
2017-10-09 22:20:23 +0900

    Project1 -Almost

---
+++ Project1/insert.sql
@@ -0,0 +1,106 @@
+USE Pokemon;
+
+INSERT INTO City (name, description) VALUES
+('Blue City', 'Waterpark'),
+('Sangnok City', 'Amazon'),
+('Brown City', 'Famous Harbor'),
+('Rainbow City', 'Huge City with Casino');
+
+INSERT INTO Trainer (name, hometown) VALUES
+('Red', 'Sangnok City'),
+('Green', 'Sangnok City'),
+('Blue', 'Sangnok City'),
+('Ash', 'Sangnok City'),
+('Yellow', 'Sangnok City'),
+('Gold', 'Blue City'),
+('Matis', 'Brown City'),
+('Erika', 'Rainbow City'),
+('Misty', 'Blue City');
+
+INSERT INTO Gym (leader_id, city) VALUES
+((SELECT id from Trainer where name='Misty'), 'Blue City'),
+((SELECT id from Trainer where name='Green'), 'Sangnok City'),
+((SELECT id from Trainer where name='Matis'), 'Brown City'),
+((SELECT id from Trainer where name='Erika'), 'Rainbow City');
+
+INSERT INTO Pokemon (id, name, type) VALUES
+(1, 'Bulbasaur', 'Grass'),
+(2, 'Ivysaur', 'Grass'),
+(3, 'Venusaur', 'Grass'),
+(4, 'Charmander', 'Fire'),
+(5, 'Charmeleon', 'Fire'),
+(6, 'Charizard', 'Fire'),
+(7, 'Squirtle', 'Water'),
+(8, 'Wartortle', 'Water'),
+(9, 'Blastoise', 'Water'),
+(17, 'Pidgeotto', 'Normal'),
+(18, 'Pidgeot', 'Normal'),
+(25, 'Pikachu', 'Electric'),
+(26, 'Raichu', 'Electric'),
+(37, 'Vulpix', 'Fire'),
+(38, 'Ninetales', 'Fire'),
+(46, 'Paras', 'Grass'),
+(47, 'Parasect', 'Grass'),
+(54, 'Psyduck', 'Water'),
+(55, 'Golduck', 'Water'),
+(84, 'Doduo', 'Normal'),
+(85, 'Dodrio', 'Normal'),
+(116, 'Horsea', 'Water'),
+(117, 'Seadra', 'Water'),
+(126, 'Magmar', 'Fire'),
+(129, 'Magikarp', 'Water'),
+(130, 'Gyarados', 'Water'),
+(131, 'Lapras', 'Water'),
+(133, 'Eevee', 'Normal'),
+(137, 'Porygon', 'Normal'),
+(150, 'Mewtwo', 'Psychic'),
+(151, 'Mew', 'Psychic'),
+(152, 'Chikorita', 'Grass'),
+(172, 'Pichu', 'Electric'),
+(249, 'Lugia', 'Psychic'),
+(251, 'Celebi', 'Psychic');
+
+INSERT INTO Evolution (before_id, after_id) VALUES
+(1,2),
+(2,3),
+(4,5),
+(5,6),
+(7,8),
+(8,9),
+(17,18),
+(25,26),
+(37,38),
+(46,47),
+(54,55),
+(84,85),
+(116,117),
+(129,130),
+(172,25);
+
+
+INSERT INTO CatchedPokemon (pid, owner_id, level, nickname) VALUES
+(6, (SELECT id from Trainer where name='Green'), 70, 'After eating hot peppers'),
+(3, (SELECT id from Trainer where name='Red'), 80, 'Funny Smell'),
+(25, (SELECT id from Trainer where name='Red'), 10, 'AAA'),
+(25, (SELECT id from Trainer where name='Ash'), 50, 'Mouse'),
+(26, (SELECT id from Trainer where name='Red'), 60, 'AA'),
+(131, (SELECT id from Trainer where name='Misty'), 80, 'Laputa'),
+(131, (SELECT id from Trainer where name='Ash'), 50, 'Turtle'),
+(133, (SELECT id from Trainer where name='Green'), 5, 'Fluffy'),
+(150, (SELECT id from Trainer where name='Red'), 100, 'Spoon killer'),
+(152, (SELECT id from Trainer where name='Gold'), 1, 'Salad'),
+(152, (SELECT id from Trainer where name='Gold'), 2, 'Piece of cake'),
+(8, (SELECT id from Trainer where name='Blue'), 20, 'Ninja Turtle'),
+(117, (SELECT id from Trainer where name='Blue'), 20, 'Water gun'),
+(38, (SELECT id from Trainer where name='Green'), 50, 'Ahri'),
+(55, (SELECT id from Trainer where name='Green'), 65, 'Donald Duck'),
+(26, (SELECT id from Trainer where name='Yellow'), 60, 'First friend'),
+(84, (SELECT id from Trainer where name='Yellow'), 3, 'Cho\'gall'),
+(85, (SELECT id from Trainer where name='Yellow'), 3, 'Three-headed dumb'),
+(46, (SELECT id from Trainer where name='Erika'), 30, 'Mushroom'),
+(152, (SELECT id from Trainer where name='Erika'), 5, 'Chikorita'),
+(130, (SELECT id from Trainer where name='Misty'), 90, 'Worm'),
+(25, (SELECT id from Trainer where name='Matis'), 40, 'Free Battery'),
+(26, (SELECT id from Trainer where name='Matis'), 50, 'Huge Battery'),
+(172, (SELECT id from Trainer where name='Matis'), 10, 'Small Battery'),
+(130, (SELECT id from Trainer where name='Red'), 95, 'Big yawn');
---
+++ Project1/project1.sql
@@ -0,0 +1,158 @@
+--Number 1
+select name
+from Trainer
+where hometown = 'Blue city';
+
+--Number 2
+select name
+from Trainer
+where hometown = 'Brown City' or hometown = 'Rainbow City';
+
+--Number3
+select name, hometown
+from Trainer
+where name like 'a%' or name like 'e%' or name like 'i%' or name like 'o%' or name like 'u%';
+
+--Number 4
+select name
+from Pokemon
+where type = 'Water';
+
+--Number 5
+select distinct type
+from Pokemon;
+
+--Number 6
+select name
+from Pokemon
+order by name asc;
+
+--Number 7
+select name
+from Pokemon
+where name like '%s';
+
+--Number 8
+select name
+from Pokemon
+where name like '%e%s';
+
+--Number 9
+select name
+from Pokemon
+where name like 'a%' or name like 'e%' or name like 'i%' or name like 'o%' or name like 'u%';
+
+--Number 10
+select type, count(*) as '#number' 
+from Pokemon
+group by type;
+
+--Number 11
+select nickname
+from CatchedPokemon
+order by level desc limit 3;
+
+--Number 12
+select avg(level)
+from CatchedPokemon;
+
+--Number 13
+select max(level) - min(level)
+from CatchedPokemon;
+
+--Number 14
+select count(*)
+from Pokemon
+where name between 'b' and 'e';
+
+--Number 15
+select count(*)
+from Pokemon
+where type not in ('Fire','Grass','Water','Electric');
+
+--Number 16
+select T.name as 'Owner name', P.name as 'Pokemon name', C.nickname as 'Pokemon nick name'
+from Trainer as T, Pokemon as P, CatchedPokemon as C
+where C.nickname like '% %' and C.owner_id = T.id and C.pid = P.id;
+
+--Number 17
+select T.name as 'Trainer name'
+from Trainer as T, Pokemon as P, CatchedPokemon as C
+where C.pid = P.id and P.type = 'Psychic' and C.owner_id = T.id;
+
+--Number 18
+select T.name, T.hometown
+from Trainer as T, CatchedPokemon as C
+where T.id = C.owner_id
+group by T.name
+order by avg(level) desc limit 3);    
+
+--Number 19
+select T.name, count(*) as '#Catched Pokemon'
+from Trainer as T, CatchedPokemon as C
+where C.owner_id = T.id
+group by T.name
+order by count(*) desc, T.name desc;
+
+--Number 20
+select P.name, C.level
+from Gym as G, Trainer as T, CatchedPokemon as C, Pokemon as P
+where G.leader_id = T.id and G.city = 'Sangnok City' and C.owner_id = T.id and C.pid = P.id
+order by C.level asc;
+
+--Number 21--check
+select P.name, count(pid) as '#Catch'
+from Pokemon as P left join CatchedPokemon as C on P.id = C.pid
+group by P.name
+order by count(pid) desc;
+
+--Number 22--Notdone
+select
+from Evolution as E, Pokemon as P
+where E.before_id = P.id
+
+--Number 23--check
+select distinct P.name
+from CatchedPokemon as C left join Pokemon as P on C.pid = P.id
+where C.pid <= 30
+order by P.name asc;
+
+--Number 24
+select T.name, P.type
+from Trainer as T, CatchedPokemon as C, Pokemon as P
+where T.id = C.owner_id and C.pid = P.id
+group by T.name
+having count(distinct P.type) = 1;
+
+--Number 25--Notdone
+select T.name, P.name, P.type
+from Trainer as T, CatchedPokemon as C, Pokemon as P
+where T.id = C.owner_id and C.pid = P.id
+
+--Number 26
+select T.name as 'Trainer name', P.name as 'Pokemon name', count(P.name) as '#Catch'
+from Trainer as T, CatchedPokemon as C, Pokemon as P
+where T.id = C.owner_id and C.pid = P.id
+group by T.name
+having count(distinct P.name) = 1;
+
+--Number 27--Notdone
+select T.name as 'Leader name', C.name as 'City name'
+from Trainer as T, Gym as G, City as C
+where G.leader_id = T.id and 
+
+--Number 28--check
+select T.name, sum(if(C.level >= 50, C.level, NULL))
+from Gym as G, Trainer as T, CatchedPokemon as C
+where G.leader_id = T.id and T.id = C.owner_id
+group by T.name
+
+--Number 29--Notdone
+select P.name
+from Trainer as T, CatchedPokemon as C, Pokemon as P
+where T.id = C.owoner_id and C.pid = P.id and (T.hometown = 'Blue city' or T.hometown = 'Sangnok City') and 
+
+--Number 30--Notdone
+select P.name
+from Pokemon as P, Evolution as E
+where P.id = E.after_id;
